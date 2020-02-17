80696fe28b9a37e3a30e3ddbe8d3159aa03047fa
David Romaní <david@flux.cat>
2020-02-17 18:30:33 +0100

    add Transformer classes

---
+++ src/Transformer/DatesTransformer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Transformer;
+
+use DateTime;
+use DateTimeInterface;
+use Exception;
+
+/**
+ * Class DatesTransformer.
+ *
+ * @category Transformer
+ *
+ * @author   David Romaní <david@flux.cat>
+ */
+class DatesTransformer
+{
+    /**
+     * @param DateTimeInterface $dateCode
+     *
+     * @return DateTime
+     *
+     * @throws Exception
+     */
+    public function convertStringWithDayAndMonthToDateTime(DateTimeInterface $dateCode)
+    {
+        $date = explode('/', $dateCode);
+        if (2 != count($date)) {
+            throw new Exception('Expected DD/MM format');
+        }
+        if (!is_numeric($date[0])) {
+            throw new Exception('Expected numeric day format');
+        }
+        if (!is_numeric($date[1])) {
+            throw new Exception('Expected numeric month format');
+        }
+        $day = new DateTime();
+        $day->setDate(0, intval($date[1]), intval($date[0]));
+        $day->setTime(0, 0, 0);
+
+        return $day;
+    }
+}
---
+++ src/Transformer/LocationsTransformer.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Transformer;
+
+use App\Enum\ConstantsEnum;
+
+/**
+ * Class LocationsTransformer.
+ *
+ * @category Transformer
+ *
+ * @author   David Romaní <david@flux.cat>
+ */
+class LocationsTransformer
+{
+    /**
+     * @param string $country
+     *
+     * @return string
+     */
+    public function countryToCode(string $country)
+    {
+        if ('ALEMANIA' == $country || 'GERMANY' == $country) {
+            $result = 'DE';
+        } elseif ('BELGICA' == $country || 'BELGIUM' == $country) {
+            $result = 'BE';
+        } elseif ('REPUBLICA CHECA' == $country || 'CZECH REPUBLIC' == $country) {
+            $result = 'CZ';
+        } elseif ('FRANCIA' == $country || 'FRANCE' == $country) {
+            $result = 'FR';
+        } elseif ('LITUANIA' == $country || 'LITHUANIA' == $country) {
+            $result = 'LT';
+        } elseif ('MARRUECOS' == $country || 'MOROCCO' == $country) {
+            $result = 'MA';
+        } elseif ('HOLANDA' == $country || 'NETHERLANDS' == $country) {
+            $result = 'NL';
+        } elseif ('POLONIA' == $country || 'POLAND' == $country) {
+            $result = 'PL';
+        } elseif ('PORTUGAL' == $country) {
+            $result = 'PT';
+        } elseif ('REINO UNIDO' == $country || 'GREAT BRITAIN' == $country || 'UNITED KINGDOM' == $country) {
+            $result = 'GB';
+        } else {
+            $result = ConstantsEnum::DEFAULT_COUNTRY_CODE_SPAIN;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return string
+     */
+    public function codeToCountry(string $code)
+    {
+        $code = strtoupper($code);
+        if ('DE' == $code) {
+            $result = 'ALEMANIA';
+        } elseif ('BE' == $code) {
+            $result = 'BELGICA';
+        } elseif ('REPUBLICA CHECA' == $code) {
+            $result = 'REPUBLICA CHECA';
+        } elseif ('FR' == $code) {
+            $result = 'FRANCIA';
+        } elseif ('LT' == $code) {
+            $result = 'LITUANIA';
+        } elseif ('MA' == $code) {
+            $result = 'MARRUECOS';
+        } elseif ('NL' == $code) {
+            $result = 'HOLANDA';
+        } elseif ('PL' == $code) {
+            $result = 'POLONIA';
+        } elseif ('PT' == $code) {
+            $result = 'PORTUGAL';
+        } elseif ('GB' == $code) {
+            $result = 'REINO UNIDO';
+        } else {
+            $result = ConstantsEnum::DEFAULT_COUNTRY_SPAIN;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $countryName
+     *
+     * @return string
+     */
+    public function countryNameCleaner(string $countryName)
+    {
+        return $this->commonNameCleaner($countryName);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function provinceNameCleaner(string $name)
+    {
+        if (strlen($name) > 0) {
+            // remove first blank character from string
+            if (' ' == substr($name, 0, 1)) {
+                $name = substr($name, 1);
+            }
+            if ('ALMERÍA' == $name || 'ALMERíA' == $name) {
+                $name = 'ALMERIA';
+            } elseif ('CASTELLO' == $name || 'CASTELLÓ' == $name || 'CASTELLó' == $name || 'CASTELLÓN' == $name || 'CASTELLóN' == $name) {
+                $name = 'CASTELLON';
+            } elseif ('CORUÑA, LA' == $name || 'CORUñA, LA' == $name || 'A CORUÑA' == $name || 'A CORUñA' == $name) {
+                $name = 'LA CORUÑA';
+            } elseif ('LLEIDA' == $name || 'LéRIDA' == $name || 'LÉRIDA' == $name) {
+                $name = 'LERIDA';
+            } elseif ('MÚRCIA' == $name || 'MúRCIA' == $name) {
+                $name = 'MURCIA';
+            } elseif ('GIRONA' == $name) {
+                $name = 'GERONA';
+            }
+        } else {
+            $name = '---';
+        }
+
+        return strtoupper($name);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function cityNameCleaner(string $name)
+    {
+        return $this->commonNameCleaner($name);
+    }
+
+    /**
+     * @param string $postalCode
+     *
+     * @return string
+     */
+    public function postalCodeCleaner(string $postalCode)
+    {
+        return $this->commonNameCleaner($postalCode);
+    }
+
+    /**
+     * @param string $taxIdentificationNumber
+     *
+     * @return string
+     */
+    public function taxIdentificationNumberCleaner(string $taxIdentificationNumber)
+    {
+        return $this->commonNameCleaner($taxIdentificationNumber);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function nameCleaner(string $name)
+    {
+        return $this->commonNameCleaner($name);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    private function commonNameCleaner(string $name)
+    {
+        if (strlen($name) > 0) {
+            // remove first blank character from string
+            if (' ' == substr($name, 0, 1)) {
+                $name = substr($name, 1);
+            }
+        } else {
+            // set blank mark
+            $name = '---';
+        }
+
+        return strtoupper($name);
+    }
+}
