a9ce9f301b992b45d9069ca6cf4d664f6306a4f3
David Romaní <david@flux.cat>
2020-02-19 20:44:11 +0100

    add NotificationsService defs

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -47,7 +47,7 @@ class NotificationService
      * @param string          $amd
      * @param string          $urlBase
      */
-    public function __construct(CourierService $messenger, EngineInterface $twig, $amd, $urlBase)
+    public function __construct(CourierService $messenger, EngineInterface $twig, string $amd, string $urlBase)
     {
         $this->messenger = $messenger;
         $this->twig = $twig;
