d07aea4b5e59efcdfbe56f27d6dd88e99db7b9d4
David Romaní <david@flux.cat>
2020-03-10 20:31:08 +0100

    fix new Doctrine migrations defs

--- src/Migrations/Version20180731161256.php
+++ src/Migrations/Version20180731161256.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Migrations;
+namespace DoctrineMigrations;
 
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
@@ -13,7 +13,7 @@ class Version20180731161256 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -30,7 +30,7 @@ class Version20180731161256 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
