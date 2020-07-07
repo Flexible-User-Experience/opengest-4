34b02ea1e53796ecdc00a2fa3b17aa5d3d3e3f50
David Romaní <david@flux.cat>
2020-07-07 18:06:43 +0200

    fix scroll to top problem

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,26 +1,14 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/frontend.less';
+const $ = require('jquery');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import jQuery from 'jquery/dist/jquery.min.js';
-import $ from 'jquery';
+global.$ = global.jQuery = $;
 
-const routes = require('../../public/js/fos_js_routes.js');
+const routes = require('../../public/js/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
-window.Dropzone = require('dropzone/dist/min/dropzone.min');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/frontend.js');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
+jQuery(document).ready(function() {
+    // jQuery('[data-toggle="popover"]').popover();
     console.log('assets/js/frontend.js on Ready trigger');
 });
 
@@ -31,10 +19,10 @@ jQuery(document).on('scroll', function() {
         jQuery('.scroll-top-wrapper').removeClass('show');
     }
 });
+
 jQuery('.scroll-top-wrapper').on('click', scrollToTop);
 
 function scrollToTop() {
-    let verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
     let element = jQuery('body');
     let offset = element.offset();
     let offsetTop = offset.top;
--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,25 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     .copyFiles([
-        {from: './assets/img'},
-        {from: './assets/svg'},
-        {from: './assets/fonts'}
-    ])
-    .copyFiles([
+        {from: './assets/img', to: 'img/[path][name].[ext]'},
+        {from: './assets/svg', to: 'svg/[path][name].[ext]'},
+        {from: './assets/fonts', to: 'fonts/[path][name].[ext]'},
         {from: './node_modules/ckeditor4/', to: 'ckeditor4/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
         {from: './node_modules/ckeditor4/adapters', to: 'ckeditor4/adapters/[path][name].[ext]'},
         {from: './node_modules/ckeditor4/lang', to: 'ckeditor4/lang/[path][name].[ext]'},
         {from: './node_modules/ckeditor4/plugins', to: 'ckeditor4/plugins/[path][name].[ext]'},
         {from: './node_modules/ckeditor4/skins', to: 'ckeditor4/skins/[path][name].[ext]'},
+        {from: './node_modules/google-charts/dist', to: 'google-charts/[path][name].[ext]'},
         {from: './node_modules/pdfjs-dist/build', to: 'pdfjs/[path][name].[ext]'}
     ])
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     */
+    .addEntry('frontend', './assets/js/frontend.js')
+    .addEntry('backend', './assets/js/backend.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -36,38 +39,18 @@ Encore
 
     /*
      * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
-    // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
 
-    // enables Less support
+    // enables LESS support
     .enableLessLoader()
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
-
-    // uncomment if you use API Platform Admin (composer req api-admin)
-    //.enableReactPreset()
-    //.addEntry('admin', './assets/js/admin.js')
-    .addEntry('frontend', './assets/js/frontend.js')
-    .addEntry('backend', './assets/js/backend.js')
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
