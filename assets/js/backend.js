bc6ff92a42d60143204344f8cf9ae9e253d97f10
David Romaní <david@flux.cat>
2020-07-08 12:09:19 +0200

    easy JS assets

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,27 +1,10 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import '../css/backend.less';
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import '../../node_modules/bootstrap-less/js/bootstrap.min';
-import $ from 'jquery';
-import 'ckeditor4/ckeditor.js';
-
-const routes = require('../../public/js/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+const routes = require('../../public/js/fos_js_routes.json');
 
 window.Dropzone = require('dropzone/dist/min/dropzone.min');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/backend.js');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-    console.log('assets/js/backend.js on Ready trigger');
-    CKEDITOR.replaceAll();
+jQuery(document).ready(function() {
+    jQuery('[data-toggle="popover"]').popover();
 });
--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,9 +1,5 @@
-// any CSS you import will output into a single css file (app.css in this case)
 import '../css/frontend.less';
 import '../../node_modules/bootstrap-less/js/bootstrap.min';
-const $ = require('jquery');
-
-global.$ = global.jQuery = $;
 
 jQuery(document).ready(function() {
     jQuery('[data-toggle="popover"]').popover();
