4a648bd58327784d9c8338d8913caf3a67967540
JSortGarcia <jordi.sort@mirmit.com>
2021-07-07 09:57:14 +0200

    Created SaleItem entity and repository

---
+++ src/Entity/Sale/SaleItem.php
@@ -0,0 +1,106 @@
+<?php
+
+
+namespace App\Entity\Sale;
+
+use App\Entity\AbstractBase;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Class SaleItem.
+ *
+ * @category
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\Sale\SaleItemRepository")
+ * @ORM\Table(name="sale_item")
+ * @UniqueEntity({"description"})
+ */
+class SaleItem extends AbstractBase
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $unitPrice;
+
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer")
+     */
+    private int $type = 0;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return SaleItem
+     */
+    public function setDescription(string $description): SaleItem
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getUnitPrice(): float
+    {
+        return $this->unitPrice;
+    }
+
+    /**
+     * @param float $unitPrice
+     *
+     * @return SaleItem
+     */
+    public function setUnitPrice(float $unitPrice): SaleItem
+    {
+        $this->unitPrice = $unitPrice;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     *
+     * @return SaleItem
+     */
+    public function setType(int $type): SaleItem
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
---
+++ src/Repository/Sale/SaleItemRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repository\Sale;
+
+use App\Entity\Enterprise\Enterprise;
+use App\Entity\Sale\SaleItem;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+
+class SaleItemRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SaleItem::class);
+    }
+
+    public function getEnabledSortedByNameQB(): QueryBuilder
+    {
+        return $this->createQueryBuilder('st')
+            ->where('st.enabled = :enabled')
+            ->setParameter('enabled', true)
+        ;
+    }
+
+    public function getEnabledSortedByNameB(): Query
+    {
+        return $this->getEnabledSortedByNameQB()->getQuery();
+    }
+
+    public function getEnabledSortedByName(): array
+    {
+        return $this->getEnabledSortedByNameB()->getResult();
+    }
+}
