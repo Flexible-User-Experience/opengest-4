27b734468ffc96e13f1531df13d1940357b076c2
JSortGarcia <jordi.sort@mirmit.com>
2021-07-20 09:23:51 +0200

    Creted migration

---
+++ migrations/Version20210720072306.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210720072306 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sale_request ADD minium_holiday_hours DOUBLE PRECISION DEFAULT NULL, ADD increase_for_holidays DOUBLE PRECISION DEFAULT NULL, ADD increase_for_holidays_percentage DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sale_request DROP minium_holiday_hours, DROP increase_for_holidays, DROP increase_for_holidays_percentage');
+    }
+}
