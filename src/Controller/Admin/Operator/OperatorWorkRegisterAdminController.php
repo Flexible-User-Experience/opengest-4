f4866704b9aeb741e0309cd027fc47b4835186c1
JSortGarcia <jordi.sort@mirmit.com>
2021-07-27 12:22:13 +0200

    Start to create OWR in custom controller

--- src/Controller/Admin/Operator/OperatorWorkRegisterAdminController.php
+++ src/Controller/Admin/Operator/OperatorWorkRegisterAdminController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin\Operator;
 
 use App\Controller\Admin\BaseAdminController;
+use App\Entity\Operator\Operator;
 use App\Entity\Operator\OperatorChecking;
+use App\Entity\Operator\OperatorWorkRegister;
 use App\Service\GuardService;
+use DateTime;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,11 +21,29 @@ class OperatorWorkRegisterAdminController extends BaseAdminController
      * @param Request $request
      *
      * @return RedirectResponse|Response
+     * @throws \Exception
      */
     public function createCustomWorkRegisterAction(Request $request)
     {
         $request = $this->resolveRequest($request);
-        dd($request->query->get('custom_date'));
+        $inputType = $request->query->get('select_input_type');
+        $operatorId = $request->query->get('custom_operator');
+        /** @var Operator $operator */
+        $operator = $this->admin->getModelManager()->find(Operator::class, $operatorId);
+        if (!$operator) {
+            throw $this->createNotFoundException(sprintf('unable to find the object with id: %s', $operatorId));
+        }
+        $date = new DateTime($request->query->get('custom_date'));
+        if ($date) {
+            $operatorWorkRegister = new OperatorWorkRegister();
+            $operatorWorkRegister->setOperator($operator);
+            $operatorWorkRegister->setDate($date);
+            if ($inputType === 'unit') {
+                $item = $request->query->get('custom_item');
+            } elseif ($inputType === 'hour') {
+
+            }
+        }
 
         return new RedirectResponse($this->generateUrl('admin_app_operator_operatorworkregister_list'));
     }
