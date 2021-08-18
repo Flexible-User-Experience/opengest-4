d451e0ff96c8ed4becb582ec96175557191fcab6
JSortGarcia <jordi.sort@mirmit.com>
2021-08-18 09:55:40 +0200

    Reordered deliveryNote lines

--- src/Admin/Sale/SaleDeliveryNoteLineAdmin.php
+++ src/Admin/Sale/SaleDeliveryNoteLineAdmin.php
@@ -40,173 +40,166 @@ class SaleDeliveryNoteLineAdmin extends AbstractBaseAdmin
     /**
      * @var array
      */
-    protected $datagridValues = array(
+    protected $datagridValues = [
         '_sort_by' => 'id',
         '_sort_order' => 'ASC',
-    );
+    ];
 
     /**
      * Methods.
-     */
-
-    /**
-     * @param FormMapper $formMapper
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
             ->with('Albarà línia', $this->getFormMdSuccessBoxArray(12))
             ->add(
-                'deliveryNote',
-                EntityType::class,
-                array(
-                    'class' => SaleDeliveryNote::class,
-                    'label' => false,
-                    'required' => true,
-                    'attr' => array(
-                        'style' => 'display:none;',
-                    ),
-                )
-            )
-            ->add(
                 'saleItem',
                 EntityType::class,
-                array(
+                [
                     'class' => SaleItem::class,
                     'label' => 'admin.label.item',
                     'required' => true,
-                )
-            )
-            ->add(
-                'units',
-                null,
-                array(
-                    'label' => 'Unitats',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'priceUnit',
-                null,
-                array(
-                    'label' => 'Preu unitat',
-                    'required' => true,
-                )
-            )
-            ->add(
-                'discount',
-                null,
-                array(
-                    'label' => 'admin.label.discount',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'iva',
-                null,
-                array(
-                    'label' => 'admin.label.iva',
-                    'required' => true,
-                    'empty_data' => (string) ConstantsEnum::IVA,
-                    'attr' => array(
-                        'placeholder' => ConstantsEnum::IVA,
-                    ),
-                )
-            )
-            ->add(
-                'irpf',
-                null,
-                array(
-                    'label' => 'admin.label.irpf',
-                    'required' => true,
-                    'empty_data' => (string) ConstantsEnum::IRPF,
-                    'attr' => array(
-                        'placeholder' => ConstantsEnum::IRPF,
-                    ),
-                )
-            )
-            ->add(
-                'total',
-                null,
-                array(
-                    'label' => 'Total',
-                    'required' => false,
-                    'disabled' => true,
-                )
+                ]
             )
             ->add(
                 'description',
                 null,
-                array(
+                [
                     'label' => 'Descripció',
                     'required' => false,
-                )
+                ]
+            )
+            ->add(
+                'units',
+                null,
+                [
+                    'label' => 'Unitats',
+                    'required' => false,
+                ]
+            )
+            ->add(
+                'priceUnit',
+                null,
+                [
+                    'label' => 'Preu unitat',
+                    'required' => true,
+                ]
+            )
+            ->add(
+                'discount',
+                null,
+                [
+                    'label' => 'admin.label.discount',
+                    'required' => false,
+                ]
+            )
+            ->add(
+                'iva',
+                null,
+                [
+                    'label' => 'admin.label.iva',
+                    'required' => true,
+                    'empty_data' => (string) ConstantsEnum::IVA,
+                    'attr' => [
+                        'placeholder' => ConstantsEnum::IVA,
+                    ],
+                ]
+            )
+            ->add(
+                'irpf',
+                null,
+                [
+                    'label' => 'admin.label.irpf',
+                    'required' => true,
+                    'empty_data' => (string) ConstantsEnum::IRPF,
+                    'attr' => [
+                        'placeholder' => ConstantsEnum::IRPF,
+                    ],
+                ]
+            )
+            ->add(
+                'deliveryNote',
+                EntityType::class,
+                [
+                    'class' => SaleDeliveryNote::class,
+                    'label' => false,
+                    'required' => true,
+                    'attr' => [
+                        'class' => 'hidden',
+                    ],
+                ]
+            )
+            ->add(
+                'total',
+                null,
+                [
+                    'label' => 'Total',
+                    'required' => false,
+                    'disabled' => true,
+                ]
             )
             ->end()
         ;
     }
 
-    /**
-     * @param DatagridMapper $datagridMapper
-     */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
             ->add(
                 'deliveryNote',
                 null,
-                array(
+                [
                     'label' => 'Albarà',
-                )
+                ]
             )
             ->add(
                 'units',
                 null,
-                array(
+                [
                     'label' => 'Unitats',
-                )
+                ]
             )
             ->add(
                 'priceUnit',
                 null,
-                array(
+                [
                     'label' => 'Preu unitat',
-                )
+                ]
             )
             ->add(
                 'total',
                 null,
-                array(
+                [
                     'label' => 'Total',
-                )
+                ]
             )
             ->add(
                 'discount',
                 null,
-                array(
+                [
                     'label' => 'Descompte',
-                )
+                ]
             )
             ->add(
                 'description',
                 null,
-                array(
+                [
                     'label' => 'Descripció',
-                )
+                ]
             )
             ->add(
                 'iva',
                 null,
-                array(
+                [
                     'label' => 'IVA',
-                )
+                ]
             )
             ->add(
                 'irpf',
                 null,
-                array(
+                [
                     'label' => 'IRPF',
-                )
+                ]
             )
         ;
     }
@@ -231,79 +224,76 @@ class SaleDeliveryNoteLineAdmin extends AbstractBaseAdmin
         return $queryBuilder;
     }
 
-    /**
-     * @param ListMapper $listMapper
-     */
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
             ->add(
                 'deliveryNote',
                 null,
-                array(
+                [
                     'label' => 'Albarà',
-                )
+                ]
             )
             ->add(
                 'units',
                 null,
-                array(
+                [
                     'label' => 'Unitats',
-                )
+                ]
             )
             ->add(
                 'priceUnit',
                 null,
-                array(
+                [
                     'label' => 'Preu unitat',
-                )
+                ]
             )
             ->add(
                 'total',
                 null,
-                array(
+                [
                     'label' => 'Total',
-                )
+                ]
             )
             ->add(
                 'discount',
                 null,
-                array(
+                [
                     'label' => 'Descompte',
-                )
+                ]
             )
             ->add(
                 'description',
                 null,
-                array(
+                [
                     'label' => 'Descripció',
-                )
+                ]
             )
             ->add(
                 'iva',
                 null,
-                array(
+                [
                     'label' => 'IVA',
-                )
+                ]
             )
             ->add(
                 'irpf',
                 null,
-                array(
+                [
                     'label' => 'IRPF',
-                )
+                ]
             )
             ->add(
                 '_action',
                 'actions',
-                array(
-                    'actions' => array(
-                        'show' => array('template' => 'admin/buttons/list__action_show_button.html.twig'),
-                        'edit' => array('template' => 'admin/buttons/list__action_edit_button.html.twig'),
-                        'delete' => array('template' => 'admin/buttons/list__action_delete_button.html.twig'),
-                    ),
+                [
+                    'actions' => [
+                        'show' => ['template' => 'admin/buttons/list__action_show_button.html.twig'],
+                        'edit' => ['template' => 'admin/buttons/list__action_edit_button.html.twig'],
+                        'delete' => ['template' => 'admin/buttons/list__action_delete_button.html.twig'],
+                    ],
                     'label' => 'Accions',
-                )
+                ]
             )
         ;
     }
