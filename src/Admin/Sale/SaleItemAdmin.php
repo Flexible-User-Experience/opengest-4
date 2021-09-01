b06105440cd53b64e1798cc19f30039bd0fe438f
JSortGarcia <jordi.sort@mirmit.com>
2021-09-01 18:50:23 +0200

    Added id to sale item

--- src/Admin/Sale/SaleItemAdmin.php
+++ src/Admin/Sale/SaleItemAdmin.php
@@ -3,14 +3,12 @@
 namespace App\Admin\Sale;
 
 use App\Admin\AbstractBaseAdmin;
-use App\Entity\Enterprise\ActivityLine;
 use App\Enum\SaleItemTypeEnum;
 use Exception;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
@@ -39,17 +37,13 @@ class SaleItemAdmin extends AbstractBaseAdmin
     /**
      * @var array
      */
-    protected $datagridValues = array(
-        '_sort_by' => 'description',
+    protected $datagridValues = [
+        '_sort_by' => 'id',
         '_sort_order' => 'ASC',
-    );
+    ];
 
     /**
      * Methods.
-     */
-
-    /**
-     * @param RouteCollection $collection
      */
     protected function configureRoutes(RouteCollection $collection)
     {
@@ -60,8 +54,6 @@ class SaleItemAdmin extends AbstractBaseAdmin
     }
 
     /**
-     * @param FormMapper $formMapper
-     *
      * @throws Exception
      */
     protected function configureFormFields(FormMapper $formMapper)
@@ -71,112 +63,120 @@ class SaleItemAdmin extends AbstractBaseAdmin
             ->add(
                 'description',
                 null,
-                array(
+                [
                     'label' => 'admin.label.description',
-                )
+                ]
             )
             ->add(
                 'unitPrice',
                 null,
-                array(
+                [
                     'label' => 'admin.label.unit_price',
-                )
+                ]
             )
             ->add(
                 'type',
                 ChoiceType::class,
-                array(
+                [
                     'choices' => SaleItemTypeEnum::getEnumArray(),
                     'label' => 'admin.label.type',
-                )
+                ]
             )
             ->end()
             ->with('Controls', $this->getFormMdSuccessBoxArray(6))
             ->add(
                 'enabled',
                 CheckboxType::class,
-                array(
+                [
                     'label' => 'Actiu',
                     'required' => false,
-                )
+                ]
             )
             ->end()
         ;
     }
 
-    /**
-     * @param DatagridMapper $datagridMapper
-     */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
             ->add(
+                'id',
+                null,
+                [
+                    'label' => 'Id',
+                ]
+            )
+            ->add(
                 'description',
                 null,
-                array(
+                [
                     'label' => 'admin.label.description',
-                )
+                ]
             )
             ->add(
                 'unitPrice',
                 null,
-                array(
+                [
                     'label' => 'admin.label.unit_price',
-                )
+                ]
             )
             ->add(
                 'type',
                 null,
-                array(
+                [
                     'label' => 'admin.label.type',
-                ),
+                ],
                 ChoiceType::class,
-                array(
+                [
                     'choices' => SaleItemTypeEnum::getEnumArray(),
-                )
+                ]
             )
         ;
     }
 
-    /**
-     * @param ListMapper $listMapper
-     */
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
             ->add(
+                'id',
+                null,
+                [
+                    'label' => 'Id',
+                ]
+            )
+            ->add(
                 'description',
                 null,
-                array(
+                [
                     'label' => 'admin.label.description',
                     'editable' => true,
-                )
+                ]
             )
             ->add(
                 'unitPrice',
                 null,
-                array(
+                [
                     'label' => 'admin.label.unit_price',
                     'editable' => true,
-                )
+                ]
             )
             ->add(
                 'type',
                 ChoiceType::class,
-                array(
+                [
                     'choices' => SaleItemTypeEnum::getEnumArray(),
                     'label' => 'admin.label.type',
-                )
+                ]
             )
             ->add(
                 '_action',
                 'actions',
-                array(
-                    'actions' => array(
-                        'edit' => array('template' => 'admin/buttons/list__action_edit_button.html.twig'),
-                    ),
+                [
+                    'actions' => [
+                        'edit' => ['template' => 'admin/buttons/list__action_edit_button.html.twig'],
+                    ],
                     'label' => 'admin.actions',
-                )
+                ]
             )
         ;
     }
