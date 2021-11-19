d8c27b4751ac993417531e8b274e6fa4a9785933
JSortGarcia <jordi.sort@mirmit.com>
2021-11-19 18:18:17 +0100

    Added new import files

--- src/Command/Vehicle/ImportVehicleCsvCommand.php
+++ src/Command/Vehicle/ImportVehicleCsvCommand.php
@@ -78,6 +78,22 @@ class ImportVehicleCsvCommand extends AbstractBaseCommand
                 $vehicle->setEnterprise($enterprise);
             }
             $vehicle->setCategory($vehicleCategory);
+            $image = $this->readColumn(13, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_chasis/', '', $image);
+                $vehicle->setChassisImage($image);
+            } else {
+                $vehicle->setChassisImage(null);
+            }
+            $image = $this->readColumn(14, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('ficha_tecnica1/', '', $image);
+                $vehicle->setTechnicalDatasheet1($image);
+            } else {
+                $vehicle->setChassisImage(null);
+            }
             $image = $this->readColumn(15, $row);
             if (strlen($image) > 0) {
                 $image = str_replace('vehiculos/', '', $image);
@@ -86,13 +102,84 @@ class ImportVehicleCsvCommand extends AbstractBaseCommand
             } else {
                 $vehicle->setMainImage(null);
             }
-            $image = $this->readColumn(13, $row);
+            $image = $this->readColumn(16, $row);
             if (strlen($image) > 0) {
                 $image = str_replace('vehiculos/', '', $image);
-                $image = str_replace('imagen_chasis/', '', $image);
-                $vehicle->setChassisImage($image);
+                $image = str_replace('imagen_tabla_carga1/', '', $image);
+                $vehicle->setLoadTable($image);
             } else {
-                $vehicle->setChassisImage(null);
+                $vehicle->setLoadTable(null);
+            }
+            $image = $this->readColumn(17, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('ficha_tecnica2/', '', $image);
+                $vehicle->setTechnicalDatasheet2($image);
+            } else {
+                $vehicle->setTechnicalDatasheet2(null);
+            }
+            $image = $this->readColumn(18, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_tabla_carga2/', '', $image);
+                $vehicle->setReachDiagram($image);
+            } else {
+                $vehicle->setReachDiagram(null);
+            }
+            $image = $this->readColumn(19, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_pemiso_circulacion/', '', $image);
+                $vehicle->setTrafficCertificate($image);
+            } else {
+                $vehicle->setTrafficCertificate(null);
+            }
+            $image = $this->readColumn(20, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $vehicle->setDimensions($image);
+            } else {
+                $vehicle->setDimensions(null);
+            }
+            $image = $this->readColumn(21, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_tarjeta_transporte/', '', $image);
+                $vehicle->setTransportCard($image);
+            } else {
+                $vehicle->setTransportCard(null);
+            }
+            $image = $this->readColumn(22, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_seguro_circulacion/', '', $image);
+                $vehicle->setTrafficInsurance($image);
+            } else {
+                $vehicle->setTrafficInsurance(null);
+            }
+            $image = $this->readColumn(23, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_itv/', '', $image);
+                $vehicle->setItv($image);
+            } else {
+                $vehicle->setItv(null);
+            }
+            $image = $this->readColumn(24, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('imagen_itc/', '', $image);
+                $vehicle->setItc($image);
+            } else {
+                $vehicle->setItc(null);
+            }
+            $image = $this->readColumn(26, $row);
+            if (strlen($image) > 0) {
+                $image = str_replace('vehiculos/', '', $image);
+                $image = str_replace('declaracion_conformidad_ce/', '', $image);
+                $vehicle->setCEDeclaration($image);
+            } else {
+                $vehicle->setCEDeclaration(null);
             }
 
             $this->em->persist($vehicle);
