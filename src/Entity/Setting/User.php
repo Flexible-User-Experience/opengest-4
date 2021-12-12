c80b7edb528ce7d52a2f7e7330b0958986841c1b
JSortGarcia <jordi.sort@mirmit.com>
2021-12-12 16:44:02 +0100

    Change sonata-userr-bundle to symfony guard

--- src/Entity/Setting/User.php
+++ src/Entity/Setting/User.php
@@ -2,37 +2,32 @@
 
 namespace App\Entity\Setting;
 
+use App\Entity\AbstractBase;
 use App\Entity\Enterprise\Enterprise;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use Doctrine\ORM\Mapping as ORM;
-use Sonata\UserBundle\Entity\BaseUser as BaseUser;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Class User.
  *
  * @category Entity
  *
- * @author   Wils Iglesias <wiglesias83@gmail.com>
+ * @author   Jordi Sort <jordi.sort@mirmit.com>
  *
  * @ORM\Entity(repositoryClass="App\Repository\Setting\UserRepository")
  * @ORM\Table(name="admin_user")
  * @Vich\Uploadable()
  */
-class User extends BaseUser
+class User extends AbstractBase implements UserInterface
 {
-    /**
-     * @var int
-     *
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\Column(type="integer")
-     */
-    protected $id;
+    public const DEFAULT_ROLE = 'ROLE_USER';
+    public const ADMIN_ROLE = 'ROLE_SUPER_ADMIN';
 
     /**
      * @var File
@@ -69,6 +64,63 @@ class User extends BaseUser
     private $enterprises;
 
     /**
+     * @ORM\Column(name="email", type="string", unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="email_canonical", type="string", unique=true)
+     */
+    private $emailCanonical;
+
+    /**
+     * @ORM\Column(name="username", type="string", unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(name="username_canonical", type="string", unique=true)
+     */
+    private $usernameCanonical;
+
+    /**
+     * @ORM\Column(name="password", type="string", nullable=true)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(name="plain_password", type="string", nullable=true)
+     */
+    private $plainPassword;
+
+    /**
+     * @ORM\Column(name="salt", type="string", nullable=true)
+     */
+    private $salt;
+
+    /**
+     * @ORM\Column(name="roles", type="json")
+     */
+    private $roles = [self::DEFAULT_ROLE];
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_login", type="datetime", nullable=true)
+     */
+    private $lastLogin;
+
+    /**
+     * @ORM\Column(name="firstname", type="string", nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(name="lastname", type="string", nullable=true)
+     */
+    private $lastname;
+
+    /**
      * Methods.
      */
 
@@ -77,7 +129,6 @@ class User extends BaseUser
      */
     public function __construct()
     {
-        parent::__construct();
         $this->enterprises = new ArrayCollection();
     }
 
@@ -100,8 +151,6 @@ class User extends BaseUser
     }
 
     /**
-     * @param File|null $mainImageFile
-     *
      * @return User
      *
      * @throws \Exception
@@ -189,8 +238,6 @@ class User extends BaseUser
     }
 
     /**
-     * @param Enterprise $enterprise
-     *
      * @return $this
      */
     public function addEnterprise(Enterprise $enterprise)
@@ -201,8 +248,6 @@ class User extends BaseUser
     }
 
     /**
-     * @param Enterprise $enterprise
-     *
      * @return $this
      */
     public function removeEnterprise(Enterprise $enterprise)
@@ -226,5 +271,133 @@ class User extends BaseUser
     public function getNaturalName()
     {
         return $this->getFirstname().' '.$this->getLastname();
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function eraseCredentials()
+    {
+        $this->plainPassword = null;
+
+        return $this;
+    }
+
+    public function setPlainPassword(string $plainPassword = null)
+    {
+        $this->plainPassword = $plainPassword;
+        $this->password = null;
+
+        return $this;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setRoles(array $roles)
+    {
+        $this->roles = [];
+
+        foreach ($roles as $role) {
+            $this->addRole($role);
+        }
+
+        return $this;
+    }
+
+    public function addRole($role)
+    {
+        if (!$role) {
+            return $this;
+        }
+
+        $role = strtoupper($role);
+
+        if (!in_array($role, $this->roles, true)) {
+            $this->roles[] = $role;
+        }
+
+        return $this;
+    }
+
+    public function removeRole($role)
+    {
+        if (false !== $key = array_search(strtoupper($role), $this->roles, true)) {
+            unset($this->roles[$key]);
+            $this->roles = array_values($this->roles);
+        }
+
+        return $this;
+    }
+
+    public function hasRole($role)
+    {
+        return in_array(strtoupper($role), $this->roles, true);
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getLastLogin()
+    {
+        return $this->lastLogin;
+    }
+
+    public function setLastLogin(DateTime $lastLogin)
+    {
+        $this->lastLogin = $lastLogin;
+
+        return $this;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->password,
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->username,
+            $this->password) = unserialize($serialized);
     }
 }
---
+++ src/Security/LoginAuthenticator.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Setting\User;
+use App\Form\LoginForm;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Guard\AuthenticatorInterface;
+
+class LoginAuthenticator extends AbstractFormLoginAuthenticator implements AuthenticatorInterface
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(
+        FormFactoryInterface $formFactory,
+        RouterInterface $router,
+        UserPasswordEncoderInterface $passwordEncoder
+    ) {
+        $this->formFactory = $formFactory;
+        $this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function supports(Request $request): bool
+    {
+        if ('/admin/login' != $request->getPathInfo() || 'POST' != $request->getMethod()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getCredentials(Request $request): array
+    {
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+
+        $data = $form->getData();
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $data['username']
+        );
+
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider): UserInterface
+    {
+        return $userProvider->loadUserByUsername($credentials['username']);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        if (!$this->passwordEncoder->isPasswordValid($user, $credentials['username'])) {
+            return false;
+        }
+
+        if (!$user->hasRole(User::ADMIN_ROLE)) {
+            throw new CustomUserMessageAuthenticationException("You don't have permission to access that page.");
+        }
+
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): RedirectResponse
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse($this->router->generate('admin_login'));
+    }
+
+    protected function getLoginUrl(): RedirectResponse
+    {
+        return new RedirectResponse($this->router->generate('admin_login'));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): RedirectResponse
+    {
+        return new RedirectResponse($this->router->generate('sonata_admin_dashboard'));
+    }
+}
