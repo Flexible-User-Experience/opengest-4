c68d1080cbf0052a56c849380ef7e60111094c61
JSortGarcia <jordi.sort@mirmit.com>
2021-12-15 17:07:59 +0100

    add vehicle maintenance pdf structure

--- src/Manager/Pdf/VehicleMaintenancePdfManager.php
+++ src/Manager/Pdf/VehicleMaintenancePdfManager.php
@@ -2,6 +2,8 @@
 
 namespace App\Manager\Pdf;
 
+use App\Entity\Vehicle\Vehicle;
+use App\Entity\Vehicle\VehicleMaintenance;
 use App\Enum\ConstantsEnum;
 use App\Service\PdfEngineService;
 use TCPDF;
@@ -42,9 +44,9 @@ class VehicleMaintenancePdfManager
     {
         // add start page
         $pdf->setMargins(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT, ConstantsEnum::PDF_PAGE_A4_MARGIN_TOP, ConstantsEnum::PDF_PAGE_A4_MARGIN_RIGHT, true);
-        $pdf->AddPage(ConstantsEnum::PDF_LANDSCAPE_PAGE_ORIENTATION, ConstantsEnum::PDF_PAGE_A4);
+        $pdf->AddPage(ConstantsEnum::PDF_PORTRAIT_PAGE_ORIENTATION, ConstantsEnum::PDF_PAGE_A4);
         $pdf->SetFont(ConstantsEnum::PDF_DEFAULT_FONT, '', 9);
-        $width = ConstantsEnum::PDF_PAGE_A4_WIDTH_LANDSCAPE - ConstantsEnum::PDF_PAGE_A4_MARGIN_RIGHT - ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT;
+        $width = ConstantsEnum::PDF_PAGE_A4_WIDTH_PORTRAIT - ConstantsEnum::PDF_PAGE_A4_MARGIN_RIGHT - ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT;
 
         // get the current page break margin
         $bMargin = $pdf->getBreakMargin();
@@ -58,6 +60,61 @@ class VehicleMaintenancePdfManager
         $pdf->setPageMark();
         // set cell padding
         $pdf->setCellPaddings(1, 1, 1, 1);
+
+        // logo
+        //TODO why the logo doesn't come up?
+        $pdf->Image($this->pdfEngineService->getSmartAssetsHelper()->getAbsoluteAssetFilePath('/bundles/app/img/logo_romani.png'), ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT, 5, 30); // TODO replace by enterprise image if defined
+
+        //TODO get the list of operators
+        $vehicles = [];
+        /** @var Vehicle $vehicles */
+        foreach ($vehicles as $vehicle) {
+            //Header
+            $pdf->setXY(30, 40);
+            $pdf->setX(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT);
+            $this->pdfEngineService->setStyleSize('b', 10);
+            $pdf->Cell(130, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Vehículo - '.$vehicle->getName(),
+                true, 0, 'L', true);
+            $pdf->Cell(40, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Matrícula - '.$vehicle->getVehicleRegistrationNumber(),
+                true, 0, 'L', true);
+            $pdf->Ln();
+            //subheader
+            $pdf->setX(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT);
+            $this->pdfEngineService->setStyleSize('', 9);
+            $pdf->Cell(60, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Mantenimiento',
+                true, 0, 'L', true);
+            $pdf->Cell(70, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Descripción',
+                true, 0, 'L', true);
+            $pdf->Cell(20, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Horas restantes',
+                true, 0, 'L', true);
+            $pdf->Cell(20, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Fecha efectuada',
+                true, 0, 'L', true);
+            //info
+            /** @var VehicleMaintenance $vehicleMaintenance */
+            foreach ($vehicleMaintenances as $vehicleMaintenance) {
+                $pdf->setX(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT);
+                $this->pdfEngineService->setStyleSize('', 9);
+                $pdf->Cell(60, ConstantsEnum::PDF_CELL_HEIGHT,
+                    $vehicleMaintenance->getVehicleMaintenanceTask(),
+                    true, 0, 'L', false);
+                $pdf->Cell(70, ConstantsEnum::PDF_CELL_HEIGHT,
+                    $vehicleMaintenance->getDescription(),
+                    true, 0, 'L', false);
+                //TODO calculate missing hours
+                $pdf->Cell(20, ConstantsEnum::PDF_CELL_HEIGHT,
+                    '',
+                    true, 0, 'L', false);
+                $pdf->Cell(20, ConstantsEnum::PDF_CELL_HEIGHT,
+                    '',
+                    true, 0, 'L', false);
+            }
+        }
 
         return $pdf;
     }
