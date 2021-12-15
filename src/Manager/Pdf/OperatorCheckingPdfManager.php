acdd0d1b5e33af167e843e0c8068c5b0611ca3a9
JSortGarcia <jordi.sort@mirmit.com>
2021-12-15 16:58:56 +0100

    edit operator checking pdf

--- src/Manager/Pdf/OperatorCheckingPdfManager.php
+++ src/Manager/Pdf/OperatorCheckingPdfManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Manager\Pdf;
 
+use App\Entity\Operator\OperatorChecking;
 use App\Enum\ConstantsEnum;
 use App\Service\PdfEngineService;
 use TCPDF;
@@ -42,9 +43,9 @@ class OperatorCheckingPdfManager
     {
         // add start page
         $pdf->setMargins(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT, ConstantsEnum::PDF_PAGE_A4_MARGIN_TOP, ConstantsEnum::PDF_PAGE_A4_MARGIN_RIGHT, true);
-        $pdf->AddPage(ConstantsEnum::PDF_LANDSCAPE_PAGE_ORIENTATION, ConstantsEnum::PDF_PAGE_A4);
+        $pdf->AddPage(ConstantsEnum::PDF_PORTRAIT_PAGE_ORIENTATION, ConstantsEnum::PDF_PAGE_A4);
         $pdf->SetFont(ConstantsEnum::PDF_DEFAULT_FONT, '', 9);
-        $width = ConstantsEnum::PDF_PAGE_A4_WIDTH_LANDSCAPE - ConstantsEnum::PDF_PAGE_A4_MARGIN_RIGHT - ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT;
+        $width = ConstantsEnum::PDF_PAGE_A4_WIDTH_PORTRAIT - ConstantsEnum::PDF_PAGE_A4_MARGIN_RIGHT - ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT;
 
         // get the current page break margin
         $bMargin = $pdf->getBreakMargin();
@@ -58,6 +59,60 @@ class OperatorCheckingPdfManager
         $pdf->setPageMark();
         // set cell padding
         $pdf->setCellPaddings(1, 1, 1, 1);
+
+        // logo
+        //TODO why the logo doesn't come up?
+        $pdf->Image($this->pdfEngineService->getSmartAssetsHelper()->getAbsoluteAssetFilePath('/bundles/app/img/logo_romani.png'), ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT, 5, 30); // TODO replace by enterprise image if defined
+
+        //TODO get the list of operators
+        $operators = [];
+        /** @var Operator $operators */
+        foreach ($operators as $operator) {
+            //Header
+            $pdf->setXY(30, 40);
+            $pdf->setX(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT);
+            $this->pdfEngineService->setStyleSize('b', 10);
+            $pdf->Cell(90, ConstantsEnum::PDF_CELL_HEIGHT,
+                $operator->getName(),
+                true, 0, 'L', true);
+            $pdf->Cell(80, ConstantsEnum::PDF_CELL_HEIGHT,
+                'D.N.I. - '.$operator->getTaxIdentificationNumber(),
+                true, 0, 'L', true);
+            $pdf->Ln();
+            //subheader
+            $pdf->setX(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT);
+            $this->pdfEngineService->setStyleSize('', 9);
+            $pdf->Cell(50, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Revisión',
+                true, 0, 'L', true);
+            $pdf->Cell(40, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Caduca',
+                true, 0, 'L', true);
+            $pdf->Cell(50, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Cita previa',
+                true, 0, 'L', true);
+            $pdf->Cell(30, ConstantsEnum::PDF_CELL_HEIGHT,
+                'Revisado',
+                true, 0, 'L', true);
+            //info
+            /** @var OperatorChecking $operatorChecking */
+            foreach ($operatorCheckings as $operatorChecking) {
+                $pdf->setX(ConstantsEnum::PDF_PAGE_A4_MARGIN_LEFT);
+                $this->pdfEngineService->setStyleSize('', 9);
+                $pdf->Cell(50, ConstantsEnum::PDF_CELL_HEIGHT,
+                    $operatorChecking->getType(),
+                    true, 0, 'L', false);
+                $pdf->Cell(40, ConstantsEnum::PDF_CELL_HEIGHT,
+                    $operatorChecking->getEnd(),
+                    true, 0, 'L', false);
+                $pdf->Cell(50, ConstantsEnum::PDF_CELL_HEIGHT,
+                    '',
+                    true, 0, 'L', false);
+                $pdf->Cell(30, ConstantsEnum::PDF_CELL_HEIGHT,
+                    '',
+                    true, 0, 'L', false);
+            }
+        }
 
         return $pdf;
     }
