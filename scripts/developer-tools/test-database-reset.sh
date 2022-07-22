3afcabfb71083a1b365d8faf36cd7e8afb375867
JSortGarcia <jordi.sort@mirmit.com>
2022-07-22 10:49:05 +0200

    Reverted to php generic command

--- scripts/developer-tools/test-database-reset.sh
+++ scripts/developer-tools/test-database-reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-php8.1 bin/console cache:clear --env=test
-php8.1 bin/console doctrine:database:drop --force --env=test
-php8.1 bin/console doctrine:database:create --env=test
-php8.1 bin/console doctrine:schema:update --force --env=test
-php8.1 bin/console hautelook:fixtures:load --no-interaction --env=test
+php bin/console cache:clear --env=test
+php bin/console doctrine:database:drop --force --env=test
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:schema:update --force --env=test
+php bin/console hautelook:fixtures:load --no-interaction --env=test
