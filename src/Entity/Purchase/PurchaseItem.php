02a09853fd9dbe09111281109ed5e3851fc63441
JSortGarcia <jordi.sort@mirmit.com>
2022-08-09 09:18:02 +0200

    Created purchas item entity

---
+++ src/Entity/Purchase/PurchaseItem.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace App\Entity\Sale;
+
+use App\Entity\AbstractBase;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Class PurchaseItem.
+ *
+ * @category
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\Purchase\PurchaseItemRepository")
+ * @ORM\Table(name="purchase_item")
+ * @UniqueEntity({"name"})
+ */
+class PurchaseItem extends AbstractBase
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private string $name;
+
+    /**
+     * @var ?string
+     *
+     * @ORM\Column(type="string", nullable = true)
+     */
+    private ?string $description;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return PurchaseItem
+     */
+    public function setName(string $name): PurchaseItem
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     *
+     * @return PurchaseItem
+     */
+    public function setDescription(?string $description): PurchaseItem
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Methods.
+     */
+    public function __toString()
+    {
+        return $this->getId().'-'.$this->getName();
+    }
+}
