b9cefc7299353cc720e995ad85dce4cf8b9effb5
JSortGarcia <jordi.sort@mirmit.com>
2022-08-09 09:43:50 +0200

    Added purchase item admin

---
+++ src/Admin/Purchase/PurchaseItemAdmin.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Admin\Purchase;
+
+use App\Admin\AbstractBaseAdmin;
+use App\Enum\SaleItemTypeEnum;
+use Exception;
+use Sonata\AdminBundle\Datagrid\DatagridInterface;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+/**
+ * Class PurchaseItemAdmin.
+ *
+ * @category    Admin
+ */
+class PurchaseItemAdmin extends AbstractBaseAdmin
+{
+    /**
+     * @var string
+     */
+    protected $classnameLabel = 'Articulos de compra';
+
+    /**
+     * @var string
+     */
+    protected $baseRoutePattern = 'compras/articulos';
+
+    /**
+     * Methods.
+     */
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        $sortValues[DatagridInterface::PAGE] = 1;
+        $sortValues[DatagridInterface::SORT_ORDER] = 'ASC';
+        $sortValues[DatagridInterface::SORT_BY] = 'name';
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        $formMapper
+            ->with('admin.with.general', $this->getFormMdSuccessBoxArray(4))
+            ->add(
+                'name',
+                null,
+                [
+                    'label' => 'admin.label.name',
+                ]
+            )
+            ->add(
+                'description',
+                null,
+                [
+                    'label' => 'admin.label.description',
+                ]
+            )
+            ->end()
+            ->with('admin.with.controls', $this->getFormMdSuccessBoxArray(6))
+            ->add(
+                'enabled',
+                CheckboxType::class,
+                [
+                    'label' => 'admin.label.enabled_male',
+                    'required' => false,
+                ]
+            )
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+    {
+        $datagridMapper
+            ->add(
+                'id',
+                null,
+                [
+                    'label' => 'Id',
+                ]
+            )
+            ->add(
+                'name',
+                null,
+                [
+                    'label' => 'admin.label.name',
+                ]
+            )
+            ->add(
+                'description',
+                null,
+                [
+                    'label' => 'admin.label.description',
+                ]
+            )
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper): void
+    {
+        $listMapper
+            ->add(
+                'id',
+                null,
+                [
+                    'label' => 'Id',
+                ]
+            )
+            ->add(
+                'name',
+                null,
+                [
+                    'label' => 'admin.label.name',
+                ]
+            )
+            ->add(
+                'description',
+                null,
+                [
+                    'label' => 'admin.label.description',
+                ]
+            )
+            ->add(
+                '_action',
+                'actions',
+                [
+                    'actions' => [
+                        'edit' => ['template' => 'admin/buttons/list__action_edit_button.html.twig'],
+                        'delete' => ['template' => 'admin/buttons/list__action_delete_button.html.twig'],
+                    ],
+                    'label' => 'admin.actions',
+                ]
+            )
+        ;
+    }
+}
