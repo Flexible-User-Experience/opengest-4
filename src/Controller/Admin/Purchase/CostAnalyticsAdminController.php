28c93daa6fa0ea67fec709c653f83a821759ae6a
JSortGarcia <jordi.sort@mirmit.com>
2022-08-18 15:49:55 +0200

    Render delivery notes and year options in imputable costs

--- src/Controller/Admin/Purchase/CostAnalyticsAdminController.php
+++ src/Controller/Admin/Purchase/CostAnalyticsAdminController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller\Admin\Purchase;
 
-use Sonata\AdminBundle\Controller\CRUDController;
+use App\Controller\Admin\BaseAdminController;
+use App\Entity\Sale\SaleDeliveryNote;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CostAnalyticsAdminController extends CRUDController
+class CostAnalyticsAdminController extends BaseAdminController
 {
     public function imputableCostsAction(Request $request): Response
     {
+        $saleDeliveryNoteRepository = $this->em->getRepository(SaleDeliveryNote::class);
+        $saleDeliveryNotes = $saleDeliveryNoteRepository->getFilteredByEnterpriseSortedById($this->getUser()->getDefaultEnterprise());
+        $oldestYear = $saleDeliveryNoteRepository->getOldestYearFilteredByEnterprise($this->getUser()->getDefaultEnterprise());
+        $currentYear = date('Y');
+        $oldestYear = $oldestYear['year'] ?? $currentYear;
+
         return $this->renderWithExtraParams(
             'admin/analytics/imputable_costs.html.twig',
-            []
+            [
+                'saleDeliveryNotes' => $saleDeliveryNotes,
+                'years' => range($currentYear, $oldestYear),
+            ]
         );
     }
 }
