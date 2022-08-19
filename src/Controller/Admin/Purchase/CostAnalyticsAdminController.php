fecfac2de96d9d13f014353d22548c131e815a1d
JSortGarcia <jordi.sort@mirmit.com>
2022-08-19 09:05:43 +0200

    Made search options for imputable costs screen

--- src/Controller/Admin/Purchase/CostAnalyticsAdminController.php
+++ src/Controller/Admin/Purchase/CostAnalyticsAdminController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin\Purchase;
 
 use App\Controller\Admin\BaseAdminController;
+use App\Entity\Operator\Operator;
 use App\Entity\Sale\SaleDeliveryNote;
+use App\Entity\Setting\CostCenter;
+use App\Entity\Vehicle\Vehicle;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,7 +14,15 @@ class CostAnalyticsAdminController extends BaseAdminController
 {
     public function imputableCostsAction(Request $request): Response
     {
+        $year = $request->get('year');
+        $saleDeliveryNoteId = $request->get('sale_delivery_note');
+        $vehicleId = $request->get('vehicle');
+        $operatorId = $request->get('operator');
+        $costCenterId = $request->get('costCenter');
         $saleDeliveryNoteRepository = $this->em->getRepository(SaleDeliveryNote::class);
+        $vehicles = $this->em->getRepository(Vehicle::class)->findEnabledSortedByName();
+        $operators = $this->em->getRepository(Operator::class)->getFilteredByEnterpriseEnabledSortedByName($this->getUser()->getDefaultEnterprise());
+        $costCenters = $this->em->getRepository(CostCenter::class)->getEnabledSortedByName();
         $saleDeliveryNotes = $saleDeliveryNoteRepository->getFilteredByEnterpriseSortedById($this->getUser()->getDefaultEnterprise());
         $oldestYear = $saleDeliveryNoteRepository->getOldestYearFilteredByEnterprise($this->getUser()->getDefaultEnterprise());
         $currentYear = date('Y');
@@ -22,6 +33,14 @@ class CostAnalyticsAdminController extends BaseAdminController
             [
                 'saleDeliveryNotes' => $saleDeliveryNotes,
                 'years' => range($currentYear, $oldestYear),
+                'vehicles' => $vehicles,
+                'operators' => $operators,
+                'costCenters' => $costCenters,
+                'selectedYear' => $year,
+                'selectedSaleDeliveryNoteId' => $saleDeliveryNoteId,
+                'selectedVehicleId' => $vehicleId,
+                'selectedOperatorId' => $operatorId,
+                'selectedCostCenterId' => $costCenterId,
             ]
         );
     }
