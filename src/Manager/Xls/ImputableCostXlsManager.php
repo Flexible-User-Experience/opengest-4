ed4c11b014bdfe51f7acdec753d067d446329b30
albamirmit <alba.tomas@mirmit.com>
2022-10-13 12:48:25 +0200

    print invoice lines and operatorworkregister on xls

--- src/Manager/Xls/ImputableCostXlsManager.php
+++ src/Manager/Xls/ImputableCostXlsManager.php
@@ -63,6 +63,64 @@ class ImputableCostXlsManager
 //            'operatorWorkRegisters' => $operatorWorkRegisters,
 //            'vehicleConsumptions' => $vehicleConsumptions,
 //        ]
+        $x = 0;
+        $spreadsheet->setActiveSheetIndex($x);
+        $activeSheet = $spreadsheet->getActiveSheet()
+            ->setTitle('Facturas de compra')
+            ->setCellValue('A1', 'Id Fra')
+            ->setCellValue('B1', 'Fecha')
+            ->setCellValue('C1', 'Unidades')
+            ->setCellValue('D1', 'Precio')
+            ->setCellValue('E1', 'Importe(€)')
+            ->setCellValue('F1', 'Artículo')
+            ->setCellValue('G1', 'Descripción')
+            ->setCellValue('H1', 'Imputado a')
+        ;
+        $i = 2;
+        /** @var PurchaseInvoiceLine $purchaseInvoiceLine */
+        foreach($parameters['purchaseInvoiceLines'] as $purchaseInvoiceLine)
+        {
+            $activeSheet = $spreadsheet->getActiveSheet()
+                ->setCellValue('A'.$i, 'Id Fra')
+                ->setCellValue('B'.$i, 'Fecha')
+                ->setCellValue('C'.$i, 'Unidades')
+                ->setCellValue('D'.$i, 'Precio')
+                ->setCellValue('E'.$i, 'Importe(€)')
+                ->setCellValue('F'.$i, 'Artículo')
+                ->setCellValue('G'.$i, 'Descripción')
+                ->setCellValue('H'.$i, 'Imputado a')
+            ;
+            $i++;
+        }
+
+        $spreadsheet->createSheet();
+        $x = 1;
+        $spreadsheet->setActiveSheetIndex($x);
+        $activeSheet = $spreadsheet->getActiveSheet()
+            ->setTitle('Partes de trabajo')
+            ->setCellValue('A1', 'Id Parte')
+            ->setCellValue('B1', 'Fecha')
+            ->setCellValue('C1', 'Unidades')
+            ->setCellValue('D1', 'Precio')
+            ->setCellValue('E1', 'Importe(€)')
+            ->setCellValue('F1', 'Albarán de venta')
+            ->setCellValue('G1', 'Descripción')
+        ;
+        $i = 2;
+        /** @var OperatorWorkRegister $operatorWorkRegister */
+        foreach($parameters['operatorWorkRegisters'] as $operatorWorkRegister)
+        {
+            $activeSheet = $spreadsheet->getActiveSheet()
+                ->setCellValue('A'.$i, $operatorWorkRegister->getId())
+                ->setCellValue('B'.$i, $operatorWorkRegister->getStart())
+                ->setCellValue('C'.$i, $operatorWorkRegister->getUnits())
+                ->setCellValue('D'.$i, $operatorWorkRegister->getPriceUnit())
+                ->setCellValue('E'.$i, $operatorWorkRegister->getAmount())
+                ->setCellValue('F'.$i, $operatorWorkRegister->getSaleDeliveryNote())
+                ->setCellValue('G'.$i, $operatorWorkRegister->getDescription())
+            ;
+            $i++;
+        }
 
         return $spreadsheet;
     }
