bcd0c01a6085a4847e275b7f23dc891d9608e7a4
JSortGarcia <jordi.sort@mirmit.com>
2022-12-27 10:36:35 +0100

    Added operatorcheckingtypegroup enum and added it in type

--- src/Entity/Operator/OperatorCheckingType.php
+++ src/Entity/Operator/OperatorCheckingType.php
@@ -5,6 +5,7 @@ namespace App\Entity\Operator;
 use App\Entity\AbstractBase;
 use App\Entity\Traits\DescriptionTrait;
 use App\Entity\Traits\NameTrait;
+use App\Enum\OperatorCheckingTypeGroupEnum;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,7 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @category Entity
  *
  * @author   Wils Iglesias <wiglesias83@gmail.com>
- * 
+ *
  * @ORM\Entity(repositoryClass="App\Repository\Operator\OperatorCheckingTypeRepository")
  * @ORM\Table(name="operator_checking_type")
  */
@@ -23,8 +24,24 @@ class OperatorCheckingType extends AbstractBase
     use DescriptionTrait;
 
     /**
+     * @ORM\Column(type="integer")
+     */
+    private OperatorCheckingTypeGroupEnum $group = OperatorCheckingTypeGroupEnum::CHECKING;
+
+    /**
      * Methods.
      */
+    public function getGroup(): OperatorCheckingTypeGroupEnum
+    {
+        return $this->group;
+    }
+
+    public function setGroup(OperatorCheckingTypeGroupEnum $group): OperatorCheckingType
+    {
+        $this->group = $group;
+
+        return $this;
+    }
 
     /**
      * @return string
