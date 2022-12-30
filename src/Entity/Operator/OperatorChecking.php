f392d16476f659ee5a4f8f44380b19e6eda04bcc
JSortGarcia <jordi.sort@mirmit.com>
2022-12-30 16:46:56 +0100

    Refactored OperatorChecking to permit extending for diferent oepratoerchecking type category

--- src/Entity/Operator/OperatorChecking.php
+++ src/Entity/Operator/OperatorChecking.php
@@ -18,7 +18,7 @@ use Symfony\Component\Validator\Context\ExecutionContextInterface;
  * @ORM\Entity(repositoryClass="App\Repository\Operator\OperatorCheckingRepository")
  * @ORM\Table(name="operator_cheking")
  */
-class OperatorChecking extends AbstractBase
+class OperatorChecking extends OperatorCheckingBase
 {
     /**
      * @var Operator
@@ -47,110 +47,4 @@ class OperatorChecking extends AbstractBase
      * @ORM\Column(type="date")
      */
     private $end;
-
-    /**
-     * Methods.
-     */
-
-    /**
-     * @return Operator
-     */
-    public function getOperator()
-    {
-        return $this->operator;
-    }
-
-    /**
-     * @param Operator $operator
-     *
-     * @return OperatorChecking
-     */
-    public function setOperator($operator)
-    {
-        $this->operator = $operator;
-
-        return $this;
-    }
-
-    /**
-     * @return OperatorCheckingType
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param OperatorCheckingType $type
-     *
-     * @return OperatorChecking
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-
-        return $this;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getBegin()
-    {
-        return $this->begin;
-    }
-
-    /**
-     * @return OperatorChecking
-     */
-    public function setBegin(DateTime $begin)
-    {
-        $this->begin = $begin;
-
-        return $this;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getEnd()
-    {
-        return $this->end;
-    }
-
-    /**
-     * @return OperatorChecking
-     */
-    public function setEnd(DateTime $end)
-    {
-        $this->end = $end;
-
-        return $this;
-    }
-
-    public function getStatus(): bool
-    {
-        return true;
-    }
-
-    /**
-     * @Assert\Callback
-     */
-    public function validate(ExecutionContextInterface $context)
-    {
-        if ($this->getEnd() < $this->getBegin()) {
-            $context
-                ->buildViolation('La data ha de ser més gran que la data d\'expedició')
-                ->atPath('end')
-                ->addViolation();
-        }
-    }
-
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return $this->id ? $this->getBegin()->format('d/m/Y').' · '.$this->getType().' · '.$this->getOperator() : '---';
-    }
 }
